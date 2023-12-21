ab1f8a30b2e5a7f828813bcda00058ce567f0758
taehyun08 <didls1643@naver.com>
2023-12-21 17:59:29 +0900

    Fix : 거래 후기 레포지토리 수정

--- src/main/java/kr/nadeuli/service/trade/TradeScheduleRepository.java
+++ src/main/java/kr/nadeuli/service/trade/TradeScheduleRepository.java
@@ -11,6 +11,6 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 
 public interface TradeScheduleRepository extends JpaRepository<TradeSchedule, Long> {
-    @Query("SELECT t FROM TradeSchedule t WHERE (t.seller = :member OR t.buyer = :member) AND t.tradingTime > :currentTime")
+    @Query("SELECT t FROM TradeSchedule t WHERE (t.buyer = :member) AND t.tradingTime > :currentTime")
     Page<TradeSchedule> findTradeScheduleList(@Param("member") Member member, @Param("currentTime") LocalDateTime currentTime, Pageable pageable);
 }
